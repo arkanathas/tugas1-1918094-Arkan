a1c7465ef237c20fda1d0f38993e4d57c083b45b
Arkan Athas <arkanathas@gmail.com>
2021-10-27 19:38:39 +0700

    tugas commit

---
+++ app/src/main/java/com/example/prak1_1918094_arkan/Segitiga.java
@@ -0,0 +1,40 @@
+package com.example.prak1_1918094_arkan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Segitiga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segitiga);
+
+        final EditText alas =
+                (EditText)findViewById(R.id.input_alas);
+        final EditText tinggi =
+                (EditText)findViewById(R.id.input_tinggi);
+
+        final TextView luas =
+                (TextView)findViewById(R.id.output_luas);
+
+
+        final Button hitung =
+                (Button)findViewById(R.id.btn_hitung);
+        hitung.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                float alas1 = Float.parseFloat(String.valueOf(alas.getText()));
+                float tinggi1 = Float.parseFloat(String.valueOf(tinggi.getText()));
+                float hasil1 = alas1*tinggi1/2;
+                luas.setText(Float.toString(hasil1));
+            }
+        });
+
+    }
+}
